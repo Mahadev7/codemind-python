0b0a2b8955febfdca7bffe4877bb41c13d96e1ab
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2022-09-28 22:11:06 +0530

    Number = int(input()) length = len(str(Number))
    Temp = Number
    Sum = 0
    rem = 0
    while Temp > 0:
        rem = Temp % 10
        Sum = Sum + int(rem**length)
        Temp = Temp // 10
        length = length - 1
    #print("The Sum of the Digits = %d" %Sum)
    if Sum == Number:
        print("True")
    else:
        print("False")

---
+++ Disarium_number.py
@@ -0,0 +1,17 @@
+Number = int(input())
+length = len(str(Number))
+
+Temp = Number
+Sum = 0
+rem = 0
+while Temp > 0:
+    rem = Temp % 10
+    Sum = Sum + int(rem**length)
+    Temp = Temp // 10
+    length = length - 1
+#print("The Sum of the Digits = %d" %Sum)
+if Sum == Number:
+    print("True")
+else:
+    print("False")
+
