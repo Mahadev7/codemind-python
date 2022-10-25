3bdda246b2ca1f92451e329a4d6d804228384fbc
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2022-10-25 12:44:48 +0530

    def Spy(num):#1124     s=0     p=1     while num>0:#1124 112 11 1  0>0{fails}         a=num%10 # 1124%10=4 2 1 1         s=s+a   # 0+4=4  6 7 8         p=p*a   #1*4=4   8  8 8         num=num//10  # 112 11  1 0     if s==p:         print("Spy Number")     else:         print("Not Spy Number") n=int(input()) c=Spy(n)

--- Spy_Number.py
+++ Spy_Number.py
@@ -1,12 +1,14 @@
-num=int(input())
-sum=0
-prod=1
-while num:
-    a=num%10
-    sum=sum+a
-    prod=prod*a
-    num=num//10
-if sum==prod:
-    print("Spy Number")
-else:
-    print("Not Spy Number")
+def Spy(num):#1124
+    s=0
+    p=1
+    while num>0:#1124 112 11 1  0>0{fails}
+        a=num%10 # 1124%10=4 2 1 1 
+        s=s+a   # 0+4=4  6 7 8
+        p=p*a   #1*4=4   8  8 8
+        num=num//10  # 112 11  1 0
+    if s==p:
+        print("Spy Number")
+    else:
+        print("Not Spy Number")
+n=int(input())
+c=Spy(n)
