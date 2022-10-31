a3543c4999e552b49cd217dbd3ac0a4f6b326c91
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2022-10-31 14:53:51 +0530

    def get_pfs(num):# num = 0     sum=0     for i in range(1,num):         if num%i==0:             sum+=i     return sum x=int(input()) y=int(input()) if get_pfs(x)==y and get_pfs(y)==x:     print("Amicable") else:     print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,14 +1,13 @@
-a=int(input())
-b=int(input())
-pa=0
-for i in range(1,a):
-    if a%i==0:
-        pa+=i
-pb=0
-for i in range(1,b):
-    if b%i==0:
-        pb+=i
-if pa==b and pb==a:
+def get_pfs(num):# num = 0
+    sum=0
+    for i in range(1,num):
+        if num%i==0:
+            sum+=i
+    return sum
+x=int(input())
+y=int(input())
+if get_pfs(x)==y and get_pfs(y)==x:
     print("Amicable")
 else:
     print("Not Amicable")
+
