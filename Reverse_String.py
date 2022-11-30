25bbad62a83edbca486f979eadd0f4b64757f6ed
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2022-11-30 19:29:34 +0530

    x=list(map(str,input().split()))# this is codemind for i in range(len(x)-1,-1,-1):#(3,-1,-1),(1,-1,-1),(7,-1,-1)     print(x[i],end=" ") #Here in the above prgm eah string evaluted with Index 0 1 2 # of this is codemind Respectively #And the work done is that printing them in them in reverse format # i.e in the 2 1 0

---
+++ Reverse_String.py
@@ -0,0 +1,7 @@
+x=list(map(str,input().split()))# this is codemind
+for i in range(len(x)-1,-1,-1):#(3,-1,-1),(1,-1,-1),(7,-1,-1)
+    print(x[i],end=" ")
+#Here in the above prgm eah string evaluted with Index 0 1 2 
+# of this is codemind Respectively
+#And the work done is that printing them in them in reverse format
+# i.e in the 2 1 0
