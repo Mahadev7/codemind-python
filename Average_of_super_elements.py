058ce249286a744e33be93b513b06d92a6373d14
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-05-22 19:19:12 +0530

    n=int(input()) x=list(map(int,input().split())) a=[] c=0 for i in x:     if i==x.count(i):         c+=1         a.append(i) s=set(a) if sum(s)==0:     avg=0 else:     avg=sum(s)/len(s) if c==0:     print("-1") else:     print("%.2f" %avg)

---
+++ Average_of_super_elements.py
@@ -0,0 +1,17 @@
+n=int(input())
+x=list(map(int,input().split()))
+a=[]
+c=0
+for i in x:
+    if i==x.count(i):
+        c+=1
+        a.append(i)
+s=set(a)
+if sum(s)==0:
+    avg=0
+else:
+    avg=sum(s)/len(s)
+if c==0:
+    print("-1")
+else:
+    print("%.2f" %avg)
