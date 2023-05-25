3151e1ab379d33dce8612e4115183c4004250630
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-05-25 20:12:56 +0530

    n=int(input()) x=list(map(int,input().split())) d={} b=[] for i in x:     if i not in d:         d[i]=1     else:         d[i]+=1 for i,j in d.items():     if j==1:         b.append(i) if len(b)==0:     print('-1') else:     for i in b:         print(i,end=' ')

--- Display_unique_values_in_an_Array.py
+++ Display_unique_values_in_an_Array.py
@@ -1,16 +1,17 @@
-n = int(input())
-arr =list(map(int,input().split()))
-d = {}
-l = []
-for i in arr:
+n=int(input())
+x=list(map(int,input().split()))
+d={}
+b=[]
+for i in x:
     if i not in d:
         d[i]=1
     else:
         d[i]+=1
 for i,j in d.items():
-    if j == 1:
-        l.append(i)
-if len(l) == 0:
-    print("-1")
+    if j==1:
+        b.append(i)
+if len(b)==0:
+    print('-1')
 else:
-    print(*l)
+    for i in b:
+        print(i,end=' ')
