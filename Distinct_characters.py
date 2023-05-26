79ecdce21bc5d5bb4aa25fa639fd81de25047e33
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-05-26 10:56:42 +0530

    n=input() n=n.lower() l=[] d={} s=[] for i in n:     if i.isalpha():         l.append(i) for i in l:     if i not in d:         d[i]=1     else:         d[i]+=1 #print(d) for i,j in d.items():     if j==1:         s.append(i) p=sorted(s) re="".join(p) print(re)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,10 +1,20 @@
-a=input()
-a=a.lower()
-b=[]
-for i in a:
-    if i!=' ':
-        b.append(i)
-b=set(b)
-b=sorted(b)
-for i in b:
-    print(i,end='')
+n=input()
+n=n.lower()
+l=[]
+d={}
+s=[]
+for i in n:
+    if i.isalpha():
+        l.append(i)
+for i in l:
+    if i not in d:
+        d[i]=1
+    else:
+        d[i]+=1
+#print(d)
+for i,j in d.items():
+    if j==1:
+        s.append(i)
+p=sorted(s)
+re="".join(p)
+print(re)
