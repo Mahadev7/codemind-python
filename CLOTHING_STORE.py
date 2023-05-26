4556ff0e2a00a0a2ea491a52ab39a43668c58db6
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-05-26 14:12:01 +0530

    n=int(input()) x=list(map(int,input().split())) d={} for i in x:     if i not in d:         d[i]=1     else:         d[i]+=1 c=0 for i,j in d.items():     if j>1:         c+=j//2 print(c)

---
+++ CLOTHING_STORE.py
@@ -0,0 +1,13 @@
+n=int(input())
+x=list(map(int,input().split()))
+d={}
+for i in x:
+    if i not in d:
+        d[i]=1
+    else:
+        d[i]+=1
+c=0
+for i,j in d.items():
+    if j>1:
+        c+=j//2
+print(c)
