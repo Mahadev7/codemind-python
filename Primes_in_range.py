ac61efb66575b77009c4406780273437bb7cc882
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-05-26 23:09:36 +0530

    def prime(i):     if i<2:         return False     for j in range(2,int(i**0.5)+1):         if i%j==0:             return 0     return 1 a=int(input()) b=int(input()) c=0 for i in range(a,b+1):     if prime(i) and i!=0:         c+=1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,4 +1,6 @@
 def prime(i):
+    if i<2:
+        return False
     for j in range(2,int(i**0.5)+1):
         if i%j==0:
             return 0
@@ -7,6 +9,6 @@ a=int(input())
 b=int(input())
 c=0
 for i in range(a,b+1):
-    if prime(i) and i!=1:
+    if prime(i) and i!=0:
         c+=1
 print(c)
