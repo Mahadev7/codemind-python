7459d36cb91d4105f95b76b46090a385df4aad71
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-05-27 11:53:31 +0530

    a=input() a=a.split() v='aeiou' c=0 p=[] for i in a:     c=0     i.lower()     for j in i:         if j in v:             c+=1         p.append(c) print(max(p))

--- maximum_vowel_count.py
+++ maximum_vowel_count.py
@@ -1,13 +1,13 @@
 a=input()
-b=a.split()
+a=a.split()
 v='aeiou'
 c=0
-l=[]
-for i in b:
+p=[]
+for i in a:
     c=0
     i.lower()
     for j in i:
         if j in v:
             c+=1
-        l.append(c)
-print(max(l))
+        p.append(c)
+print(max(p))
