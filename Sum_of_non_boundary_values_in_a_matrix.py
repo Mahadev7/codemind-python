f7f2a9a137b9a686f5185ff1265ab0c67b6a5b95
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-05-27 10:24:34 +0530

    r,c=map(int,input().split()) m=[] for i in range(r):     x=list(map(int,input().split()))     m.append(x) p=0 for i in range(1,r-1):     for j in range(1,c-1):         p+=m[i][j] print(p)

--- Sum_of_non_boundary_values_in_a_matrix.py
+++ Sum_of_non_boundary_values_in_a_matrix.py
@@ -3,8 +3,8 @@ m=[]
 for i in range(r):
     x=list(map(int,input().split()))
     m.append(x)
-s=0
+p=0
 for i in range(1,r-1):
     for j in range(1,c-1):
-        s+=m[i][j]
-print(s)
+        p+=m[i][j]
+print(p)
