86a80a184ad0e044c8e2e9cecf59ee6f59d5374b
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-05-27 15:57:28 +0530

    a=input() b=a.split() l=[] for i in b:     i=i.lower()     s=list(i)     l.append(set(s)) z=l[0] for i in range(0,len(l)):     z=z.intersection(l[i]) if len(z)==0:     print('-1') else:     print(min(z))

--- Common_characters_-_3.py
+++ Common_characters_-_3.py
@@ -3,12 +3,12 @@ b=a.split()
 l=[]
 for i in b:
     i=i.lower()
-    c=list(i)
-    l.append(set(c))
-x=l[0]
+    s=list(i)
+    l.append(set(s))
+z=l[0]
 for i in range(0,len(l)):
-    x=x.intersection(l[i])
-if len(x)==0:
+    z=z.intersection(l[i])
+if len(z)==0:
     print('-1')
 else:
-    print(*min(x))
+    print(min(z))
