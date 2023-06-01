18f57f15ed93978a460aae6820201d9130b50c5c
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-06-01 13:26:49 +0530

    a,b=map(int,input().split()) x,y=[],[] c=[] for i in range(1,a+1):     if a%i==0:         # print(i)         x.append(i) for i in range(1,b+1):     if b%i==0:         y.append(i) for i in x:     if  i in y:         c.append(i) print(max(c))

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,7 +1,14 @@
-a,b = map(int,input().split())
-c = max(a,b)
-l=[]
-for i in range(1,c+1):
-    if a%i==0 and b%i == 0:
-        l.append(i)
-print(max(l))
+a,b=map(int,input().split())
+x,y=[],[]
+c=[]
+for i in range(1,a+1):
+    if a%i==0:
+        # print(i)
+        x.append(i)
+for i in range(1,b+1):
+    if b%i==0:
+        y.append(i)
+for i in x:
+    if  i in y:
+        c.append(i)
+print(max(c))
