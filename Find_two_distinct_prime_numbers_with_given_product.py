d684c7695b6f79759ad274b8547cf036eb94b95c
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-06-01 12:21:37 +0530

    def prime(n):     if n<2:         return False     for i in range(2,n):         if n%i==0:             return False     return True n=int(input()) l=[] for i in range(2,n):     if n%i ==0 and prime(i):         l.append(i) if len(l)<2:     print(-1) else:     print(*l)

--- Find_two_distinct_prime_numbers_with_given_product.py
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -1,20 +1,16 @@
-def prime(s):
-    # if s<2:
-    #     return False
-    for i in range(2,s):
-        if s%i==0:
+def prime(n):
+    if n<2:
+        return False
+    for i in range(2,n):
+        if n%i==0:
             return False
     return True
-a=int(input())
-p=[]
-for i in range(2,a):
-    for j in range(2,a):
-        if prime(i) and prime(j):
-            if i*j==a:
-                p.append(i)
-                p.append(j)
-if sum(p)==0:
-    print("-1")
+n=int(input())
+l=[]
+for i in range(2,n):
+    if n%i ==0 and prime(i):
+        l.append(i)
+if len(l)<2:
+    print(-1)
 else:
-    x=set(p)
-    print(*x)
+    print(*l)
