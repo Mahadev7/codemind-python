1b786c423283a607575d28d95efc67e16fda761c
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-06-02 22:39:28 +0530

    a=input() x=0 y=0 for i in a:     if i=='U':         x+=1     if i=='D':         x-=1     if i=='L':         y+=1     if i=='R':         y-=1 if x==0 and y==0:     print("True") else:     print("False")

--- Robot_Return_to_Origin.py
+++ Robot_Return_to_Origin.py
@@ -1,11 +1,7 @@
 a=input()
 x=0
 y=0
-l=[]
 for i in a:
-    if i.isalpha() and(i=='U' or i=='D' or i=='L' or i=='R'):
-        l.append(i)
-for i in l:
     if i=='U':
         x+=1
     if i=='D':
