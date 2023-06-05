9ba87e3a822bde5cf4e84e7edafa3eb83c78e336
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-06-05 14:17:38 +0530

    n=int(input()) x=list(map(int,input().split())) a=[] b=[] c=n//2 for i in range(c):     a.append(x[i]) for i in range(c,n):     b.append(x[i]) b=b[::-1] print(*(b+a))

--- Half_List.py
+++ Half_List.py
@@ -1,11 +1,11 @@
 n=int(input())
 x=list(map(int,input().split()))
+a=[]
+b=[]
 c=n//2
-l1=[]
-l2=[]
 for i in range(c):
-    l1.append(x[i])
+    a.append(x[i])
 for i in range(c,n):
-    l2.append(x[i])
-l3 = l2[::-1]
-print(*(l3+l1))
+    b.append(x[i])
+b=b[::-1]
+print(*(b+a))
