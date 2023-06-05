6eec2b3fe580f55be5073196aee601643edd3374
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-06-05 23:21:29 +0530

    def add(n):     s=0     while n:         p=n%10         s+=p         n=n//10     return s x=int(input()) while x>9:     x=add(x) print(x)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,12 +1,11 @@
-def isdigit(n):
+def add(n):
     s=0
     while n:
         p=n%10
         s+=p
         n=n//10
     return s
-n=int(input())
-p=len(str(n))
-while n>9:
-    n=isdigit(n)
-print(n)
+x=int(input())
+while x>9:
+    x=add(x)
+print(x)
