ec3bda0bcbe7676bb340331de7f676009016afb4
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-06-05 23:19:05 +0530

    def isdigit(n):     s=0     while n:         p=n%10         s+=p         n=n//10     return s n=int(input()) p=len(str(n)) while n>9:     n=isdigit(n) print(n)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,14 +1,12 @@
-def adddigits(num):
+def isdigit(n):
     s=0
-    while num>0:
-        d=num%10
-        num=num//10
-        s=s+d
+    while n:
+        p=n%10
+        s+=p
+        n=n//10
     return s
-
-
-
 n=int(input())
+p=len(str(n))
 while n>9:
-    n=adddigits(n)
+    n=isdigit(n)
 print(n)
