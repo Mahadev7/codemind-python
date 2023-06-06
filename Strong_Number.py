29a83edb8b2cccebe7632cecbce746971b8af872
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-06-06 10:21:05 +0530

    def fact(n):     f=1     for i in range(1,n+1):         f*=i     return f a=input() s=0 l=list(a) for i in l:     i=int(i)     s+=fact(i) if s==int(a):     print(f"The number {a} is a strong number") else:     print(f"The number {a} is not a strong number")

--- Strong_Number.py
+++ Strong_Number.py
@@ -3,14 +3,14 @@ def fact(n):
     for i in range(1,n+1):
         f*=i
     return f
-    
-n=input()
-l=list(n)
+a=input()
 s=0
+l=list(a)
 for i in l:
     i=int(i)
     s+=fact(i)
-if s==int(n):
-    print(f"The number {n} is a strong number")
+if s==int(a):
+    print(f"The number {a} is a strong number")
 else:
-    print(f"The number {n} is not a strong number")
+    print(f"The number {a} is not a strong number")
+
