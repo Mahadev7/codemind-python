79a6cb542251929db86d9ae6e1d939c6e913aaa2
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-06-06 14:39:03 +0530

    a=input() d={} for i in a:     # 1 1 2 3     i=int(i)     if i not in d:         d[i]=1 # '1':1     else:         d[i]+=1 s1,s2=0,0 for i,j in d.items():     s1+=1     if j==1:         s2+=1 if s1==s2:     print("Unique Number") else:     print("Not Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,22 +1,19 @@
-def fun(d,num): # (8,0) #(2,8)
-    while num:
-        k=num%10 #0 #8%10=8
-        num=num//10 #0 # 0
-        if d==k:#8==0 #2==
-            return True
-    return False
-
-
-
-
-n=int(input())#28
-s=0
-while n:
-    d=n%10 # 8 # 2
-    n=n//10 # 2 #0
-    if fun(d,s):# (8,0) #(2,8)
-        print("Not Unique Number")
-        break
-    s=s*10+d # 8
-else:
+a=input()
+d={}
+for i in a:
+    # 1 1 2 3 
+    i=int(i)
+    if i not in d:
+        d[i]=1 # '1':1
+    else:
+        d[i]+=1
+s1,s2=0,0
+for i,j in d.items():
+    s1+=1
+    if j==1:
+        s2+=1
+if s1==s2:
     print("Unique Number")
+else:
+    print("Not Unique Number")
+        
