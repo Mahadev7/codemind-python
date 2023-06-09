1c21f994d5a12ef912b70b947eb976fb4b2ebbcd
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-06-09 10:32:18 +0530

    a=input() b=list(a) c=[] for i in range(len(b)):     for j in range(i,len(b)):         s=b[i:j+1]         c.append(s) # print(c) d=0 for i in c:     if i==i[::-1]:         if len(i)>d:             d=len(i)             s=i print(''.join(s))

--- Longest_Palindromic_Substring.py
+++ Longest_Palindromic_Substring.py
@@ -6,11 +6,10 @@ for i in range(len(b)):
         s=b[i:j+1]
         c.append(s)
 # print(c)
-d=[]
-c1=0
+d=0
 for i in c:
     if i==i[::-1]:
-        if len(i)>c1: # 
-            c1=len(i)
-            p=i
-print(''.join(p))
+        if len(i)>d:
+            d=len(i)
+            s=i
+print(''.join(s))
