691814eb692faadb2e91708a92531bcf6c83869d
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-06-15 22:21:57 +0530

    a=list(map(str,input().split())) c=0 v='aeiou' c='bcdfghjklmnpqrstvwxyz' s=0 for i in a:     i=i.lower()     if (i[0] in v) and (i[-1] in c):         s+=1 print(s)

--- count_words.py
+++ count_words.py
@@ -1,7 +1,10 @@
 a=list(map(str,input().split()))
 c=0
+v='aeiou'
+c='bcdfghjklmnpqrstvwxyz'
+s=0
 for i in a:
     i=i.lower()
-    if (i[0]=='a' or i[0]=='e' or i[0]=='i' or i[0]=='o' or i[0]=='u') and (i[-1]!='a' or i[-1]!='e' or i[-1]!='i' or i[-1]!='o' or i[-1]!='u'):
-        c+=1                                                                  
-print(c)
+    if (i[0] in v) and (i[-1] in c):
+        s+=1
+print(s)
