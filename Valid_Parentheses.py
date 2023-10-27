ac4db3aeb2a17e2d75e40616ce9b85cfb5d62682
Yaswanth Kumar <98527340+Mahadev7@users.noreply.github.com>
2023-10-27 23:28:33 +0530

    def Balanced(n):     a = '{[('     b = '}])'     s = []     for i in n:         if i == '{' or i == '[' or i == '(':             s.append(i)         else:             if len(s)==0:                 return False             if a.index(s[-1])==b.index(i):                 s.pop()     if len(s)==0:         return True     return False for _ in range(int(input())):         n = input()         print(Balanced(n))

---
+++ Valid_Parentheses.py
@@ -0,0 +1,18 @@
+def Balanced(n):
+    a = '{[('
+    b = '}])'
+    s = []
+    for i in n:
+        if i == '{' or i == '[' or i == '(':
+            s.append(i)
+        else:
+            if len(s)==0:
+                return False
+            if a.index(s[-1])==b.index(i):
+                s.pop()
+    if len(s)==0:
+        return True
+    return False
+for _ in range(int(input())):
+        n = input()
+        print(Balanced(n))
